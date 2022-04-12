081650c2d443febcf7b4a189316444e82a1f61c8
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-12 17:09:47 +0200

    Sesto esercizio terminato

--- jsnack6/script.js
+++ jsnack6/script.js
@@ -2,3 +2,14 @@
  JSnack 6
  Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
 */
+
+const numeriUtente = parseInt(prompt('Inserire numeri di cui eseguire la potenza'))
+let potenzaNumeri;
+
+for(let i = 1; i <= numeriUtente; i++){
+
+  
+  potenzaNumeri = i*i*i
+  document.getElementById('potenze').innerHTML += `<h1> ${potenzaNumeri} </h1>`
+  console.log(potenzaNumeri);
+}
