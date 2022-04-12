d07c24d3eea66b207ed285ff9dcf9b1216d93aab
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-12 16:50:01 +0200

    Quinto esercizio finito

--- jsnack5/script.js
+++ jsnack5/script.js
@@ -4,3 +4,16 @@
  Chiedi per 6 volte all’utente di inserire un numero,
  se è dispari inseriscilo nell’array
 */
+
+const array = [];
+
+for(let i = 0; i < 6; i++){
+  const userNumber = parseInt(prompt('Inserire un numero'))
+  if(userNumber % 2){
+    array.push(userNumber)
+  }
+}
+
+document.getElementById('array').innerHTML = `Dati i numeri inseriti, avrai come array finale : [${array}}`;
+
+console.log('questo è l\'array che avremo dopo l\'inserimento dei numeri: ' , array );
