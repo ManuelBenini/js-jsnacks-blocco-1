73f9b89b4c8d725f4f5e715cc923deb2567f29d6
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-12 17:28:23 +0200

    Settimo esercizio terminato

--- jsnack7/script.js
+++ jsnack7/script.js
@@ -5,3 +5,12 @@
  perciò per fermarsi prima di 1000 bisogna stampare la potenza prima di incrementarla
  perché altrimenti il controllo viene fatto dopo e si sfora
 */
+
+const moltiplicazione = 2;
+let numeroBase = 2;
+
+do{
+  document.getElementById('potenze').innerHTML += ` {${numeroBase}}`
+  console.log('--', numeroBase);
+  numeroBase *= moltiplicazione
+}while(numeroBase < 1000);
