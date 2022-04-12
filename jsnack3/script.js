691db7ebebd187656e980ea26aac6f34e5f1d6d2
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-12 16:00:01 +0200

    Terzo esercizio terminato

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -4,3 +4,44 @@
  Il programma stampa la somma di tutti i numeri inseriti.
  Esegui questo programma in due versioni, con il for e con il while (facoltativo)
 */
+
+
+// ESERCIZIO CON FOR
+const forCycleBtn = document.getElementById('forcycle');
+
+forCycleBtn.addEventListener('click' , function(){
+  let somma = 0;
+  
+  for(let i = 0; i < 5; i++){
+  
+    let numeriUtente = parseInt(prompt('Inserire numero'));
+    somma = parseInt(somma + numeriUtente);
+    console.log(numeriUtente);
+  }
+  
+  document.getElementById('somma').innerHTML = somma;
+  console.log(somma);
+
+})
+
+
+// ESERCIZIO CON WHILE
+const whileCycleBtn = document.getElementById('whilecycle');
+
+whileCycleBtn.addEventListener('click' , function(){
+  let somma = 0;
+
+  let contatore = 0;
+  
+  while(contatore < 5){
+  
+    contatore++;
+    let numeriUtente = parseInt(prompt('Inserire numero'));
+    somma = parseInt(somma + numeriUtente);
+    console.log(numeriUtente);
+  }
+  
+  document.getElementById('somma_2').innerHTML = somma;
+  console.log(somma);
+
+})
