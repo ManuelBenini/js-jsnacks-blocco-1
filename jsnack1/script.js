16331c1101e6748b37106ee2fb6f1018a28b75f2
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-12 15:13:41 +0200

    Primo esercizio terminato

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,3 +2,52 @@
  JSnack 1
  L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
 */
+let firstNumber;
+let secondNumber;
+let firstNumberFlag = false;
+let secondNumberFlag = false;
+
+while(firstNumberFlag === false || secondNumberFlag === false){
+  firstNumber = prompt('Inserisci il primo numero');
+  secondNumber = prompt('Inserisci il secondo numero');
+
+  if(!isNaN(firstNumber)){
+    firstNumberFlag = true
+  }else{
+    firstNumberFlag = false;
+  }
+  console.log('first number', firstNumberFlag , '----->>', firstNumber);
+  
+  if(!isNaN(secondNumber)){
+    secondNumberFlag = true
+  }else{
+    secondNumberFlag = false;
+  }
+  console.log('second number', secondNumberFlag, '----->>', secondNumber);
+
+  if(firstNumberFlag === false && secondNumberFlag === true){
+    alert('Non hai inserito un numero nel primo caso, riprovare');
+  }else if(firstNumberFlag === true && secondNumberFlag === false){
+    alert('Non hai inserito un numero nel secondo caso, riprovare');
+  }else if(firstNumberFlag === false && secondNumberFlag === false){
+    alert('Non hai inserito un numero in nessun caso, riprovare');
+  }
+  
+}
+
+console.log('I numeri inseriti sono corretti!')
+
+if(firstNumber > secondNumber){
+  document.getElementById('numero_maggiore').innerHTML = `
+  Il numero più grande tra i due inseriti &egrave;: ${firstNumber}`;
+  console.log('Il maggiore è: ', firstNumber , '(il primo)');
+}else if(firstNumber == secondNumber){
+  document.getElementById('numero_maggiore').innerHTML = `
+  I due numeri sono uguali`;
+  console.log('I due numeri inseriti sono uguali!');
+}else{
+  document.getElementById('numero_maggiore').innerHTML = `
+  Il numero più grande tra i due inseriti &egrave;: ${secondNumber}`;
+  console.log('Il maggiore è: ', secondNumber , '(il secondo)');
+}
+
