eb5c48d60c26179540906a9b0d7448d19804520c
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-12 18:54:39 +0200

    Nono esercizio terminato

--- jsnack9/script.js
+++ jsnack9/script.js
@@ -2,3 +2,18 @@
  JSnack 9
  Calcola la somma e la media dei primi 10 numeri.
 */
+
+const numeri = 10;
+let sommaTot = 0;
+let media = 0;
+
+for(let i = 1; i <= numeri; i++){
+  console.log('Numeri da 1 a 10', i);
+  sommaTot += i
+  console.log('Numeri sommati', sommaTot);
+}
+
+media = sommaTot / numeri
+console.log('Numeri divisi', media);
+
+document.getElementById('somma').innerHTML = `La somma dei primi 10 numeri &egrave; ${sommaTot}, mentre la media &egrave; ${media}.`
