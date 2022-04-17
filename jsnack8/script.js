b36ccac0d1cc19d207562c220a51fa1dc53654c1
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-17 23:37:29 +0200

    Aggiunti controlli

--- jsnack8/script.js
+++ jsnack8/script.js
@@ -7,13 +7,44 @@
  e recuperare una cifra alla volta usanto il ciclo for per scorrere la stringa numerica
 */
 
-const numeroUtente = prompt('Inserire numero');
+let numeroUtente = prompt('Inserire numero di 4 cifre');
+let numberFlag = false;
+let lenghtFlag = false;
+
+while(numberFlag === false || lenghtFlag === false){
+  if(isNaN(numeroUtente)){
+    numberFlag = false;
+    alert('Non hai inserito dei numeri, riprovare');
+    numeroUtente = prompt('Inserire numero di 4 cifre');
+  }else{
+    numberFlag = true;
+  } 
+
+  if(numeroUtente.length < 4){
+    lenghtFlag = false;
+    alert('Non hai inserito numero di 4 cifre, riprovare');
+    numeroUtente = prompt('Inserire numero di 4 cifre');
+  }else if(numeroUtente.length > 4){
+    lenghtFlag = false;
+    alert('Hai inserito numero maggiore di 4 cifre, riprovare');
+    numeroUtente = prompt('Inserire numero di 4 cifre');
+  }else{
+    lenghtFlag = true;
+  } 
+}
+
 const arrayNumeri = [];
 let sommaCifre = 0;
 
-for(let i = 1; i <= numeroUtente.length; i++){
+for(let i = 0; i < numeroUtente.length; i++){
+  arrayNumeri.push(numeroUtente[i]);
+  console.log('numeri' , numeroUtente[i]);
+  console.log('array' , arrayNumeri[i]);
+
+  /* OPPURE 
   arrayNumeri.push(numeroUtente.substring(i, i - 1));
   console.log(arrayNumeri);
+  */
 }
 
 for(let i = 0; i < arrayNumeri.length; i++){
