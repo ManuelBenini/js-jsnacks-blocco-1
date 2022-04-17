c49991ca248113f02417cd2a08a8290e319b482a
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-17 23:17:15 +0200

    Aggiunti controlli e corretto il codice

--- jsnack6/script.js
+++ jsnack6/script.js
@@ -3,13 +3,38 @@
  Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
 */
 
-const numeriUtente = parseInt(prompt('Inserire numeri di cui eseguire la potenza'))
+let numeriUtente = parseInt(prompt('Inserire numeri di cui eseguire la potenza'))
+
+while(isNaN(numeriUtente)){
+  alert('Non hai inserito un numero riprovare')
+  numeriUtente = parseInt(prompt('Inserire numeri di cui eseguire la potenza'))
+}
+
 let potenzaNumeri;
 
-for(let i = 1; i <= numeriUtente; i++){
+let x = 1
+while(x <= numeriUtente){
+  potenzaNumeri = Math.pow(x, 3);
+  x++
+  document.getElementById('potenze').innerHTML += `<h1> ${potenzaNumeri} </h1>`
+  console.log('I numeri sono:' ,potenzaNumeri);
+}
 
-  
-  potenzaNumeri = i*i*i
+/* OPPURE 
+let x = 1
+while(x <= numeriUtente){
+  potenzaNumeri = x*x*x
+  x++
+  document.getElementById('potenze').innerHTML += `<h1> ${potenzaNumeri} </h1>`
+  console.log('I numeri sono:' ,potenzaNumeri);
+}
+*/
+
+
+/*  OPPURE
+for(let i = 1; i <= numeriUtente; i++){
+  potenzaNumeri = i*i*i 
   document.getElementById('potenze').innerHTML += `<h1> ${potenzaNumeri} </h1>`
   console.log(potenzaNumeri);
 }
+*/
